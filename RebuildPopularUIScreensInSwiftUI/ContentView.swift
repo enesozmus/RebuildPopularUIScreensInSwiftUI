ea7526c9ef1b2f8c00b8d8f47d6d9ac00f5093c0
enesozmus <ensozmus@gmail.com>
2024-06-08 17:51:19 +0300

    Initial Commit

---
+++ RebuildPopularUIScreensInSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RebuildPopularUIScreensInSwiftUI
+//
+//  Created by enesozmus on 8.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
